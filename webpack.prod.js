2c19e9059d212835da11b3676896f58601cc2ccd
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-11-23 04:06:02 -0600

    Add creation of webpack-bundle-analyzer on build

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,16 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   plugins: [
-    // new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'webpack-bundle-analyzer-report.html',
+      openAnalyzer: false
+    }),
     new UglifyJsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
