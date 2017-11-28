51192c08ce556adea006672437ed9c3ac0a444ff
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-11-28 00:26:47 -0600

    Use fetch() to load map

--- src/external/map/start.ts
+++ src/external/map/start.ts
@@ -3,17 +3,10 @@ import mapManager from 'src/session/map-manager';
 import mapPath from './start.map.json';
 
 export default () => {
-  const map = mapManager.get(mapPath);
-  if (map) {
-    console.log('cheese?');
-  } else {
-    const request = new XMLHttpRequest();
-    request.responseType = 'json';
-    request.onload = () => {
-      const loadedMap = new GameMap(mapPath, request.response);
-      mapManager.set(mapPath, loadedMap);
-    };
-    request.open('GET', mapPath);
-    request.send();
-  }
-};
+  fetch(mapPath).then((response) => {
+    return response.json();
+  }).then((obj) => {
+    const loadedMap = new GameMap(mapPath, obj);
+    mapManager.set(mapPath, loadedMap);
+  });
+}
