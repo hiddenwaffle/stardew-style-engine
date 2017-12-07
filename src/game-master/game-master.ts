127b91f06a9727a1b9a0025c9559a509611d3321
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-07 15:31:05 -0600

    Account for map edges

--- src/game-master/game-master.ts
+++ src/game-master/game-master.ts
@@ -44,45 +44,63 @@ class GameMaster {
 
   private checkCollision(world: World, entity: Entity, dx: number, dy: number) {
     let solidCollision = false;
-    let solidCollisionLayer = null;
+    let solidCollisionXValue = 0;
+    let solidCollisionYValue = 0;
+
     const xprojected = entity.x + dx;
     const yprojected = entity.y + dy;
     const xprojectedTile = Math.floor(xprojected / TARGET_FIELD_TILE_SIZE);
     const yprojectedTile = Math.floor(yprojected / TARGET_FIELD_TILE_SIZE);
 
-    for (const layer of world.staticMap.collisionLayers) {
-      const index = xprojectedTile + (yprojectedTile * layer.width);
-      const value = layer.tiles[index];
-      if (value !== 0) {
-        if (layer.passthrough) {
-          console.log('i do not block you');
-        } else {
-          console.log('i block you');
-          solidCollision = true;
-          solidCollisionLayer = layer;
-          break;
-        }
+    const pushingAgainstLeftOrRightOfMap = xprojectedTile < 0 || xprojectedTile >= world.staticMap.width;
+    const pushingAgainstTopOrBottomOfMap = yprojectedTile < 0 || yprojectedTile >= world.staticMap.height;
+
+    if (pushingAgainstLeftOrRightOfMap || pushingAgainstTopOrBottomOfMap) {
+      solidCollision = true;
+      if (pushingAgainstLeftOrRightOfMap) {
+        solidCollisionXValue = 1;
       }
-    };
-    if (solidCollision) {
-      // Allow the entity to "slide" against the collision tile (when attmpting to move diagonally).
-      const xcurrentTile = Math.floor(entity.x / TARGET_FIELD_TILE_SIZE);
-      const ycurrentTile = Math.floor(entity.y / TARGET_FIELD_TILE_SIZE);
-      if (dx !== 0) {
-        const index = xprojectedTile + (ycurrentTile * solidCollisionLayer.width);
-        const value = solidCollisionLayer.tiles[index];
-        if (value === 0) {
-          entity.x = xprojected;
-        }
-      }
-      if (dy !== 0) {
-        const index = xcurrentTile + (yprojectedTile * solidCollisionLayer.width);
-        const value = solidCollisionLayer.tiles[index];
-        if (value === 0) {
-          entity.y = yprojected;
-        }
+      if (pushingAgainstTopOrBottomOfMap) {
+        solidCollisionYValue = 1;
       }
     } else {
+      for (const layer of world.staticMap.collisionLayers) {
+        const index = xprojectedTile + (yprojectedTile * layer.width);
+        const value = layer.tiles[index];
+        if (value !== 0) {
+          if (layer.passthrough) {
+            console.log('i do not block you');
+          } else {
+            console.log('i block you');
+            solidCollision = true;
+
+            // Allow the entity to "slide" against the collision tile (when attmpting to move diagonally).
+            const xcurrentTile = Math.floor(entity.x / TARGET_FIELD_TILE_SIZE);
+            const ycurrentTile = Math.floor(entity.y / TARGET_FIELD_TILE_SIZE);
+            if (dx !== 0) {
+              const index = xprojectedTile + (ycurrentTile * layer.width);
+              solidCollisionXValue = layer.tiles[index];
+            }
+            if (dy !== 0) {
+              const index = xcurrentTile + (yprojectedTile * layer.width);
+              solidCollisionYValue = layer.tiles[index];
+            }
+
+            break;
+          }
+        }
+      };
+    }
+
+    if (solidCollision) {
+      if (solidCollisionXValue === 0) {
+        entity.x = xprojected;
+      }
+      if (solidCollisionYValue === 0) {
+        entity.y = yprojected;
+      }
+    } else {
+      // TODO: Align sides together so the entity doesn't pass through
       entity.x = xprojected;
       entity.y = yprojected;
     }
