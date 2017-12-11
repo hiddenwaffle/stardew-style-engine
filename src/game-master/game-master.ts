b6f125f7f886b031d6a1a70a53c8bbed2303f858
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-11 10:18:43 -0600

    Remove redundant intersect check

--- src/game-master/game-master.ts
+++ src/game-master/game-master.ts
@@ -106,21 +106,16 @@ function walk(world: World, entity: Entity, speed: number) {
       const topTile    =  yTileToCheck      * TARGET_FIELD_TILE_SIZE;
       const bottomTile = (yTileToCheck + 1) * TARGET_FIELD_TILE_SIZE;
 
-      const intersected = intersect(
+      // Move the entity out of a solid tile.
+      const [xpush, ypush] = calculatePush(
         left, right, top, bottom,
         leftTile, rightTile, topTile, bottomTile
       );
-
-      if (intersected) {
+      const overlapped = xpush !== 0 || ypush !== 0;
+      if (overlapped) {
         // TODO: Queue scripts, if any.
-        // console.log(layer.name);
-
-        // Move the entity out of a solid tile.
+        console.log(layer.name);
         if (!layer.passthrough) {
-          const [xpush, ypush] = calculatePush(
-            left, right, top, bottom,
-            leftTile, rightTile, topTile, bottomTile
-          );
           entity.x += xpush;
           entity.y += ypush;
         }
@@ -129,21 +124,6 @@ function walk(world: World, entity: Entity, speed: number) {
   }
 
   // TODO: If solid collision and moving non-diagonal and if 'sliding' should be applied.
-}
-
-/**
- * https://stackoverflow.com/a/2752387
- */
-function intersect(
-  left1: number, right1: number, top1: number, bottom1: number,
-  left2: number, right2: number, top2: number, bottom2: number
-) {
-  return !(
-    left2   > right1  ||
-    right2  < left1   ||
-    top2    > bottom1 ||
-    bottom2 < top1
-  );
 }
 
 /**
@@ -173,13 +153,12 @@ function calculatePush(
   let xoff = 0;
   let yoff = 0;
 
-  // Yet another intersection check (TODO: Refactor so this is the only one?)
   if (xintersect < 0 && yintersect < 0) {
     if (xintersect > yintersect) {
       xoff = xdelta > 0 ? -xintersect : xintersect;
     } else if (xintersect < yintersect) {
       yoff = ydelta > 0 ? -yintersect : yintersect;
-    } else { // xintersect === yinteresect.
+    } else { // Remaining case is: xintersect === yinteresect.
       // NOTE: Needed to catch this case to prevent getting stuck on
       // two rectangles right next to each other (like how the tiles are)
     }
