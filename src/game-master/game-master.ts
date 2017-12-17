8bf7e2caadc90d6e9d41f789424eecb54e13bb88
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-17 00:07:44 -0600

    Pull entity-to-entity collision into own source file

--- src/game-master/game-master.ts
+++ src/game-master/game-master.ts
@@ -1,8 +1,6 @@
 import World from 'src/domain/world';
-import walkEntity from './walk-entity';
-import {
-  ScriptCall
-} from './script-call';
+import walkEntityToTiles from './walk-entity-to-tiles';
+import walkEntityToEntity from './walk-entity-to-entity';
 
 class GameMaster {
   /**
@@ -25,11 +23,11 @@ class GameMaster {
 
     world.entities.forEach((entity) => {
       entity.clearExpiredCallTimers();
-      const walkResult = walkEntity(world, entity);
+      const walkResult = walkEntityToTiles(world, entity);
       walkResult.executeCalls(world);
     });
 
-    checkEntityToEntityOverlap(world);
+    walkEntityToEntity(world);
   }
 
   setPlayerIntendedDirection(dxIntended: number, dyIntended: number) {
@@ -39,60 +37,3 @@ class GameMaster {
 }
 
 export default new GameMaster();
-
-/**
- * Warning: O(n^2)
- * Warning: Uses the same bounding box calculation as walk-entity.ts
- */
-function checkEntityToEntityOverlap(world: World) {
-  for (const entity of world.entities) {
-    // Calculate bounding box -- center x to middle and y to bottom.
-    const left    = entity.x - entity.boundingWidth / 2;
-    const right   = entity.x + entity.boundingWidth / 2;
-    const top     = entity.y - entity.boundingHeight;
-    const bottom  = entity.y + 1; // +1 to prevent entity's y to be on a solid tile directly below the entity.
-
-    for (const other of world.entities) {
-      if (entity.id !== other.id) {
-        // NOTE: Should be similar to calculation above for the subject entity.
-        // Calculate bounding box -- center x to middle and y to bottom.
-        const leftOther   = other.x - other.boundingWidth / 2;
-        const rightOther  = other.x + other.boundingWidth / 2;
-        const topOther    = other.y - other.boundingHeight;
-        const bottomOther = other.y + 1; // +1 to prevent entity's y to be on a solid tile directly below the entity.
-
-        if (intersect(
-          left, right, top, bottom,
-          leftOther, rightOther, topOther, bottomOther)
-        ) {
-          if (entity.entityToEntityCollisionCall) {
-            const call = new ScriptCall(
-              entity.entityToEntityCollisionCall,
-              entity.id,
-              other.id
-            );
-            if (entity.tryScriptCall(call, entity.entityToEntityCollisionCallInterval)) {
-              call.execute(world);
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-/**
- * AABB Collision (Without Response)
- * Based on: https://stackoverflow.com/a/2752387
- */
-function intersect(
-  left1: number, right1: number, top1: number, bottom1: number,
-  left2: number, right2: number, top2: number, bottom2: number
-): boolean {
-  return (
-    left2   < right1  &&
-    right2  > left1   &&
-    top2    < bottom1 &&
-    bottom2 > top1
-  );
-}
---
+++ src/game-master/walk-entity-to-entity.ts
@@ -0,0 +1,59 @@
+import World from 'src/domain/world';
+import { ScriptCall } from './script-call';
+
+/**
+ * Warning: O(n^2)
+ * Warning: Uses the same bounding box calculation as walk-entity.ts
+ */
+export default (world: World) => {
+  for (const entity of world.entities) {
+    // Calculate bounding box -- center x to middle and y to bottom.
+    const left    = entity.x - entity.boundingWidth / 2;
+    const right   = entity.x + entity.boundingWidth / 2;
+    const top     = entity.y - entity.boundingHeight;
+    const bottom  = entity.y + 1; // +1 to prevent entity's y to be on a solid tile directly below the entity.
+
+    for (const other of world.entities) {
+      if (entity.id !== other.id) {
+        // NOTE: Should be similar to calculation above for the subject entity.
+        // Calculate bounding box -- center x to middle and y to bottom.
+        const leftOther   = other.x - other.boundingWidth / 2;
+        const rightOther  = other.x + other.boundingWidth / 2;
+        const topOther    = other.y - other.boundingHeight;
+        const bottomOther = other.y + 1; // +1 to prevent entity's y to be on a solid tile directly below the entity.
+
+        if (intersect(
+          left, right, top, bottom,
+          leftOther, rightOther, topOther, bottomOther)
+        ) {
+          if (entity.entityToEntityCollisionCall) {
+            const call = new ScriptCall(
+              entity.entityToEntityCollisionCall,
+              entity.id,
+              other.id
+            );
+            if (entity.tryScriptCall(call, entity.entityToEntityCollisionCallInterval)) {
+              call.execute(world);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+/**
+ * AABB Collision (Without Response)
+ * Based on: https://stackoverflow.com/a/2752387
+ */
+function intersect(
+  left1: number, right1: number, top1: number, bottom1: number,
+  left2: number, right2: number, top2: number, bottom2: number
+): boolean {
+  return (
+    left2   < right1  &&
+    right2  > left1   &&
+    top2    < bottom1 &&
+    bottom2 > top1
+  );
+}
