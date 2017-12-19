9d6bff91ad561054b61e71f18ff7e85d6f11cd06
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-19 17:25:12 -0600

    Convert bard-white json into calculated function

--- src/session/entity-animation-manager.ts
+++ src/session/entity-animation-manager.ts
@@ -1,18 +1,12 @@
 import { EntityAnimationGroup } from 'src/domain/entity-animation';
 import imageLoader from './image-loader';
 
-import pirate     from 'src/external/entity-animation-group/pirate.json';
-import bard       from 'src/external/entity-animation-group/bard.json';
-import bardWhite  from 'src/external/entity-animation-group/bard-white.json';
-
 class EntityAnimationManager {
   private readonly groups: Map<string, EntityAnimationGroup>;
 
   constructor() {
     this.groups = new Map();
-    this.groups.set('pirate',     genGroup(pirate));
-    this.groups.set('bard',       genGroup(bard));
-    this.groups.set('bard-white', genGroup(bardWhite));
+    this.groups.set('bard-white', genGroup(genAntifareaRawGroup(13, 9)));
   }
 
   get(animationGroupName: string) {
@@ -30,4 +24,139 @@ export default new EntityAnimationManager();
 
 function genGroup(raw: any): EntityAnimationGroup {
   return new EntityAnimationGroup(raw);
+}
+
+/**
+ * Should match the structure of *.json files.
+ */
+function genAntifareaRawGroup(xoff: number, yoff: number): any {
+  return {
+    filename: 'antifarea_18x20chars.png',
+    animations: genAntifareaRawAnimations(xoff, yoff)
+  };
+}
+
+/**
+ * Should match the structure of the *.json files.
+ */
+function genAntifareaRawAnimations(xoff: number, yoff: number): any[] {
+  const rawAnimations: any[] = [];
+  const runDelay = 100;
+
+  rawAnimations.push(
+    genRawAnimation(
+      'default', 'default',
+      [
+        genRawFrame(xoff + 1, yoff + 2) // Same as stand-down.
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'stand-up', 'stand-up',
+      [
+        genRawFrame(xoff + 1, yoff)
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'stand-down', 'stand-down',
+      [
+        genRawFrame(xoff + 1, yoff + 2)
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'stand-left', 'stand-left',
+      [
+        genRawFrame(xoff + 1, yoff + 1, null, true)
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'stand-right', 'stand-right',
+      [
+        genRawFrame(xoff + 1, yoff + 1)
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'run-up', 'run-up',
+      [
+        genRawFrame(xoff + 1, yoff, runDelay),
+        genRawFrame(xoff,     yoff, runDelay),
+        genRawFrame(xoff + 1, yoff, runDelay),
+        genRawFrame(xoff,     yoff, runDelay)
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'run-down', 'run-down',
+      [
+        genRawFrame(xoff + 1, yoff + 2, runDelay),
+        genRawFrame(xoff,     yoff + 2, runDelay),
+        genRawFrame(xoff + 1, yoff + 2, runDelay),
+        genRawFrame(xoff,     yoff + 2, runDelay)
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'run-left', 'run-left',
+      [
+        genRawFrame(xoff + 1, yoff + 1, runDelay, true),
+        genRawFrame(xoff,     yoff + 1, runDelay, true),
+        genRawFrame(xoff + 1, yoff + 1, runDelay, true),
+        genRawFrame(xoff,     yoff + 1, runDelay, true)
+      ]
+    )
+  );
+  rawAnimations.push(
+    genRawAnimation(
+      'run-right', 'run-right',
+      [
+        genRawFrame(xoff + 1, yoff + 1, runDelay),
+        genRawFrame(xoff,     yoff + 1, runDelay),
+        genRawFrame(xoff + 1, yoff + 1, runDelay),
+        genRawFrame(xoff,     yoff + 1, runDelay)
+      ]
+    )
+  );
+
+  return rawAnimations;
+}
+
+/**
+ * Should match the structure of the *.json files.
+ */
+function genRawAnimation(name: string, next: string, rawFrames: any[]) {
+  return {
+    name,
+    next,
+    frames: rawFrames
+  };
+}
+
+/**
+ * Should match the structure of the *.json files.
+ */
+function genRawFrame(
+  x: number,
+  y: number,
+  delay?: number,
+  flipped?: boolean
+): any {
+  const rawFrame: any = { x, y };
+  if (delay) {
+    rawFrame.delay = delay;
+  }
+  if (flipped) {
+    rawFrame.flipped = flipped;
+  }
+  return rawFrame;
 }
