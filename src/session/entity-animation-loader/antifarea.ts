a440a7a3f365e187bf13ecfc62b4e65c01d54129
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-20 14:10:20 -0600

    Simplify Antifarea animation loading

--- src/session/entity-animation-loader/antifarea.ts
+++ src/session/entity-animation-loader/antifarea.ts
@@ -10,99 +10,98 @@ export default (): Map<string, EntityAnimationGroup> => {
 
   const groups: Map<string, EntityAnimationGroup> = new Map();
 
-  groups.set('af-monk-1',            genGroup(genAntifareaRawGroup(left,    0 * groupHeight)));
-  groups.set('af-monk-2',            genGroup(genAntifareaRawGroup(right,   0 * groupHeight)));
+  groups.set('af-monk-1',            genGroup(left,    0 * groupHeight));
+  groups.set('af-monk-2',            genGroup(right,   0 * groupHeight));
 
-  groups.set('af-fighter-silver',    genGroup(genAntifareaRawGroup(left,    1 * groupHeight)));
-  groups.set('af-fighter-pink',      genGroup(genAntifareaRawGroup(right,   1 * groupHeight)));
+  groups.set('af-fighter-silver',    genGroup(left,    1 * groupHeight));
+  groups.set('af-fighter-pink',      genGroup(right,   1 * groupHeight));
 
-  groups.set('af-vagrant-blue',      genGroup(genAntifareaRawGroup(left,    2 * groupHeight)));
-  groups.set('af-vagrant-purple',    genGroup(genAntifareaRawGroup(right,   2 * groupHeight)));
+  groups.set('af-vagrant-blue',      genGroup(left,    2 * groupHeight));
+  groups.set('af-vagrant-purple',    genGroup(right,   2 * groupHeight));
 
-  groups.set('af-holy-blue',         genGroup(genAntifareaRawGroup(left,    3 * groupHeight)));
-  groups.set('af-holy-white',        genGroup(genAntifareaRawGroup(right,   3 * groupHeight)));
+  groups.set('af-holy-blue',         genGroup(left,    3 * groupHeight));
+  groups.set('af-holy-white',        genGroup(right,   3 * groupHeight));
 
-  groups.set('af-ninja-dark',        genGroup(genAntifareaRawGroup(left,    4 * groupHeight)));
-  groups.set('af-ninja-light',       genGroup(genAntifareaRawGroup(right,   4 * groupHeight)));
+  groups.set('af-ninja-dark',        genGroup(left,    4 * groupHeight));
+  groups.set('af-ninja-light',       genGroup(right,   4 * groupHeight));
 
-  groups.set('af-forester-blonde',   genGroup(genAntifareaRawGroup(left,    5 * groupHeight)));
-  groups.set('af-forester-red',      genGroup(genAntifareaRawGroup(right,   5 * groupHeight)));
+  groups.set('af-forester-blonde',   genGroup(left,    5 * groupHeight));
+  groups.set('af-forester-red',      genGroup(right,   5 * groupHeight));
 
-  groups.set('af-sage-bald',         genGroup(genAntifareaRawGroup(left,    6 * groupHeight)));
-  groups.set('af-sage-gray',         genGroup(genAntifareaRawGroup(right,   6 * groupHeight)));
+  groups.set('af-sage-bald',         genGroup(left,    6 * groupHeight));
+  groups.set('af-sage-gray',         genGroup(right,   6 * groupHeight));
 
-  groups.set('af-caveman-messy',     genGroup(genAntifareaRawGroup(left,    7 * groupHeight)));
-  groups.set('af-caveman-bun',       genGroup(genAntifareaRawGroup(right,   7 * groupHeight)));
+  groups.set('af-caveman-messy',     genGroup(left,    7 * groupHeight));
+  groups.set('af-caveman-bun',       genGroup(right,   7 * groupHeight));
 
-  groups.set('af-caveman-short',     genGroup(genAntifareaRawGroup(left,    8 * groupHeight)));
-  groups.set('af-caveman-long',      genGroup(genAntifareaRawGroup(right,   8 * groupHeight)));
+  groups.set('af-caveman-short',     genGroup(left,    8 * groupHeight));
+  groups.set('af-caveman-long',      genGroup(right,   8 * groupHeight));
 
-  groups.set('af-dark-knight-black', genGroup(genAntifareaRawGroup(left,    9 * groupHeight)));
-  groups.set('af-dark-knight-green', genGroup(genAntifareaRawGroup(right,   9 * groupHeight)));
+  groups.set('af-dark-knight-black', genGroup(left,    9 * groupHeight));
+  groups.set('af-dark-knight-green', genGroup(right,   9 * groupHeight));
 
-  groups.set('af-soldier-green',     genGroup(genAntifareaRawGroup(left,   10 * groupHeight)));
-  groups.set('af-soldier-blue',      genGroup(genAntifareaRawGroup(right,  10 * groupHeight)));
+  groups.set('af-soldier-green',     genGroup(left,   10 * groupHeight));
+  groups.set('af-soldier-blue',      genGroup(right,  10 * groupHeight));
 
-  groups.set('af-villager-pants',    genGroup(genAntifareaRawGroup(left,   11 * groupHeight)));
-  groups.set('af-villager-dress',    genGroup(genAntifareaRawGroup(right,  11 * groupHeight)));
+  groups.set('af-villager-pants',    genGroup(left,   11 * groupHeight));
+  groups.set('af-villager-dress',    genGroup(right,  11 * groupHeight));
 
-  groups.set('af-villager-bieber',   genGroup(genAntifareaRawGroup(left,   12 * groupHeight)));
-  groups.set('af-villager-bows',     genGroup(genAntifareaRawGroup(right,  12 * groupHeight)));
+  groups.set('af-villager-bieber',   genGroup(left,   12 * groupHeight));
+  groups.set('af-villager-bows',     genGroup(right,  12 * groupHeight));
 
-  groups.set('af-clergy-hat',        genGroup(genAntifareaRawGroup(left,   13 * groupHeight)));
-  groups.set('af-clergy-nun',        genGroup(genAntifareaRawGroup(right,  13 * groupHeight)));
+  groups.set('af-clergy-hat',        genGroup(left,   13 * groupHeight));
+  groups.set('af-clergy-nun',        genGroup(right,  13 * groupHeight));
 
-  groups.set('af-pirate-red',        genGroup(genAntifareaRawGroup(left,   14 * groupHeight)));
-  groups.set('af-pirate-hat',        genGroup(genAntifareaRawGroup(right,  14 * groupHeight)));
+  groups.set('af-pirate-red',        genGroup(left,   14 * groupHeight));
+  groups.set('af-pirate-hat',        genGroup(right,  14 * groupHeight));
 
-  groups.set('af-samurai-helm',      genGroup(genAntifareaRawGroup(left,   15 * groupHeight)));
-  groups.set('af-samurai-crown',     genGroup(genAntifareaRawGroup(right,  15 * groupHeight)));
+  groups.set('af-samurai-helm',      genGroup(left,   15 * groupHeight));
+  groups.set('af-samurai-crown',     genGroup(right,  15 * groupHeight));
 
-  groups.set('af-royalty-crown',     genGroup(genAntifareaRawGroup(left,   16 * groupHeight)));
-  groups.set('af-royalty-dress',     genGroup(genAntifareaRawGroup(right,  16 * groupHeight)));
+  groups.set('af-royalty-crown',     genGroup(left,   16 * groupHeight));
+  groups.set('af-royalty-dress',     genGroup(right,  16 * groupHeight));
 
-  groups.set('af-turban',            genGroup(genAntifareaRawGroup(left,   17 * groupHeight)));
-  groups.set('af-bunny',             genGroup(genAntifareaRawGroup(right,  17 * groupHeight)));
+  groups.set('af-turban',            genGroup(left,   17 * groupHeight));
+  groups.set('af-bunny',             genGroup(right,  17 * groupHeight));
 
-  groups.set('af-bard',              genGroup(genAntifareaRawGroup(left,   18 * groupHeight)));
-  groups.set('af-dancer',            genGroup(genAntifareaRawGroup(right,  18 * groupHeight)));
+  groups.set('af-bard',              genGroup(left,   18 * groupHeight));
+  groups.set('af-dancer',            genGroup(right,  18 * groupHeight));
 
-  groups.set('af-sage-beard',        genGroup(genAntifareaRawGroup(left,   19 * groupHeight)));
-  groups.set('af-sage-white',        genGroup(genAntifareaRawGroup(right,  19 * groupHeight)));
+  groups.set('af-sage-beard',        genGroup(left,   19 * groupHeight));
+  groups.set('af-sage-white',        genGroup(right,  19 * groupHeight));
 
-  groups.set('af-villager-orange',   genGroup(genAntifareaRawGroup(left,   20 * groupHeight)));
-  groups.set('af-villager-pink',     genGroup(genAntifareaRawGroup(right,  20 * groupHeight)));
+  groups.set('af-villager-orange',   genGroup(left,   20 * groupHeight));
+  groups.set('af-villager-pink',     genGroup(right,  20 * groupHeight));
 
-  groups.set('af-vampire',           genGroup(genAntifareaRawGroup(left,   21 * groupHeight)));
-  groups.set('af-nurse',             genGroup(genAntifareaRawGroup(right,  21 * groupHeight)));
+  groups.set('af-vampire',           genGroup(left,   21 * groupHeight));
+  groups.set('af-nurse',             genGroup(right,  21 * groupHeight));
 
-  groups.set('af-shrouded-1',        genGroup(genAntifareaRawGroup(left,   22 * groupHeight)));
-  groups.set('af-shrouded-2',        genGroup(genAntifareaRawGroup(right,  22 * groupHeight)));
+  groups.set('af-shrouded-1',        genGroup(left,   22 * groupHeight));
+  groups.set('af-shrouded-2',        genGroup(right,  22 * groupHeight));
 
-  groups.set('af-elemental-fire',    genGroup(genAntifareaRawGroup(left,   23 * groupHeight)));
-  groups.set('af-elemental-ice',     genGroup(genAntifareaRawGroup(right,  23 * groupHeight)));
+  groups.set('af-elemental-fire',    genGroup(left,   23 * groupHeight));
+  groups.set('af-elemental-ice',     genGroup(right,  23 * groupHeight));
 
-  groups.set('af-elemental-earth',   genGroup(genAntifareaRawGroup(left,   24 * groupHeight)));
-  groups.set('af-elemental-plant',   genGroup(genAntifareaRawGroup(right,  24 * groupHeight)));
+  groups.set('af-elemental-earth',   genGroup(left,   24 * groupHeight));
+  groups.set('af-elemental-plant',   genGroup(right,  24 * groupHeight));
 
-  groups.set('af-elemental-light',   genGroup(genAntifareaRawGroup(left,   25 * groupHeight)));
-  groups.set('af-elemental-shadow',  genGroup(genAntifareaRawGroup(right,  25 * groupHeight)));
+  groups.set('af-elemental-light',   genGroup(left,   25 * groupHeight));
+  groups.set('af-elemental-shadow',  genGroup(right,  25 * groupHeight));
 
   return groups;
 };
 
-function genGroup(raw: any): EntityAnimationGroup {
-  return new EntityAnimationGroup(raw);
-}
-
 /**
  * Should match the structure of *.json files.
+ *
+ * Uses 'filename' instead of 'filenames' because
+ * Antifarea sprite animations do not span multiple sheets.
  */
-function genAntifareaRawGroup(xoff: number, yoff: number): any {
-  return {
+function genGroup(xoff: number, yoff: number): any {
+  return new EntityAnimationGroup({
     filename: 'antifarea_18x20chars.png',
     animations: genAntifareaRawAnimations(xoff, yoff)
-  };
+  });
 }
 
 /**
