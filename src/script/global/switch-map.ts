f2ba8d41ef0f5bd2d11e002cf29d49c6cd6a68cd
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-22 00:07:48 -0600

    Remove outdated comment

--- src/script/global/switch-map.ts
+++ src/script/global/switch-map.ts
@@ -2,7 +2,6 @@ import { log } from 'src/log';
 import { World } from 'src/domain/world';
 import { StaticMap } from 'src/domain/static-map';
 
-// TODO: Mirrors stage-manager.applySave()
 export function switchMap(mapName: string, entranceName: string, world: World) {
   world.switchMap(mapName, entranceName);
 };
