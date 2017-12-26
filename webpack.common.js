61a37b5f07837223ac08c19ede7b7cff13f7d60f
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-25 23:51:31 -0600

    Factor CSS into own file by adding loaders

--- webpack.common.js
+++ webpack.common.js
@@ -30,6 +30,10 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.png$/,
         use: fileLoaderConfig
       },
