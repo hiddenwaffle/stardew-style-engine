27b79f6abd9ff584d0fd14469d90e472e0d53abc
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-26 00:55:54 -0600

    Factor meta directory out to variable

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,16 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const metaDirectory = 'meta';
 
 module.exports = merge(common, {
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
-      reportFilename: 'meta/webpack-bundle-analyzer-report.html',
+      reportFilename: `${metaDirectory}/webpack-bundle-analyzer-report.html`,
       openAnalyzer: false
     }),
     new UglifyJsPlugin(),
