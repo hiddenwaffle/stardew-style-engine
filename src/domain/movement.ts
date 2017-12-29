01359164bb6835df6ba2bc943b9ea1086a22e683
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2017-12-29 15:45:44 -0600

    Add support structures for movement plans

--- src/domain/movement.ts
+++ src/domain/movement.ts
@@ -1,4 +1,8 @@
 import { log } from 'src/log';
+import {
+  determineDirection,
+  Direction,
+} from './direction';
 
 export enum MovementType {
   Stationary,
@@ -11,11 +15,25 @@ export function asMovementType(value: string): MovementType {
   return MovementType[value as keyof typeof MovementType];
 }
 
+class MovementTarget {
+  x: number;
+  y: number;
+  ttl: number;
+  call: string;
+}
+
 export class MovementPlan {
   private _type: MovementType;
 
+  previousDirection: Direction;
+  currentDirection: Direction;
+  targets: MovementTarget[];
+
   constructor(type?: MovementType) {
     this._type = type || MovementType.Stationary;
+    this.previousDirection = Direction.None;
+    this.currentDirection = Direction.None;
+    this.targets = [];
   }
 
   get type(): MovementType {
--- src/game-master/calculate-dx-dy-intended.ts
+++ src/game-master/calculate-dx-dy-intended.ts
@@ -1,6 +1,7 @@
 import { World } from 'src/domain/world';
 import { Entity } from 'src/domain/entity';
 import { MovementType } from 'src/domain/movement';
+import { timer } from 'src/session/timer';
 
 export function calculateDxDyIntended(world: World, entity: Entity) {
   switch (entity.movementPlan.type) {
